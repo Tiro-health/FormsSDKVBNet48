b32831898f92071ce4a1c6cc13efe55364652b82
thibault.mahieu <thibault.mahieu94@gmail.com>
2026-01-14 04:01:12 +0100

    Allow form to be closed (even when form could not be submitted)

--- FormsSDKVBNet48/Form1.vb
+++ FormsSDKVBNet48/Form1.vb
@@ -77,6 +77,7 @@ Public Class Form1
         If Not isFormSubmitted Then
             e.Cancel = True
             Await TiroFormViewer.SendFormRequestSubmitAsync()
+            isFormSubmitted = True
         End If
     End Sub
 
